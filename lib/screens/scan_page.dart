4f18e920c7c4e7eefd90e5fa69b1dfb0d7448229
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-08-17 00:45:16 +0600

    Card droppable section done

--- lib/screens/scan_page.dart
+++ lib/screens/scan_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:virtual_visiting_card/utils/constant.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -16,10 +17,26 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> {
   bool isScanOver = false;
   List<String> lines = [];
+
+  String name = '',
+      mobile = '',
+      website = '',
+      designtaion = '',
+      email = '',
+      address = '',
+      image = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Scan Page')),
+      appBar: AppBar(
+        title: Text('Scan Page'),
+        actions: [
+          IconButton(
+            onPressed: image.isEmpty ? null : () {},
+            icon: Icon(Icons.arrow_forward),
+          ),
+        ],
+      ),
       body: ListView(
         children: [
           Row(
@@ -41,6 +58,40 @@ class _ScanPageState extends State<ScanPage> {
               ),
             ],
           ),
+          if (isScanOver)
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    DropTargetItem(
+                      property: ContactProperties.name,
+                      onDrop: getPropertyValue,
+                    ),
+                    DropTargetItem(
+                      property: ContactProperties.mobile,
+                      onDrop: getPropertyValue,
+                    ),
+                    DropTargetItem(
+                      property: ContactProperties.website,
+                      onDrop: getPropertyValue,
+                    ),
+                    DropTargetItem(
+                      property: ContactProperties.designation,
+                      onDrop: getPropertyValue,
+                    ),
+                    DropTargetItem(
+                      property: ContactProperties.email,
+                      onDrop: getPropertyValue,
+                    ),
+                    DropTargetItem(
+                      property: ContactProperties.address,
+                      onDrop: getPropertyValue,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           Wrap(children: lines.map((e) => LineItem(line: e)).toList()),
         ],
       ),
@@ -50,6 +101,9 @@ class _ScanPageState extends State<ScanPage> {
   void getImage(ImageSource camera) async {
     final xFile = await ImagePicker().pickImage(source: camera);
     if (xFile != null) {
+      setState(() {
+        image = xFile.path;
+      });
       EasyLoading.show(status: 'Please wait');
       final textRecognizer = TextRecognizer(
         script: TextRecognitionScript.latin,
@@ -64,10 +118,12 @@ class _ScanPageState extends State<ScanPage> {
       }
       setState(() {
         lines = tempList;
-        isScanOver = false;
+        isScanOver = true;
       });
     }
   }
+
+  getPropertyValue(String property, String value) {}
 }
 
 class LineItem extends StatelessWidget {
@@ -89,6 +145,69 @@ class LineItem extends StatelessWidget {
       ),
       data: line,
       child: Center(child: Chip(label: Text(line))),
+    );
+  }
+}
+
+class DropTargetItem extends StatefulWidget {
+  const DropTargetItem({
+    super.key,
+    required this.property,
+    required this.onDrop,
+  });
+  final String property;
+  final Function(String, String) onDrop;
+
+  @override
+  State<DropTargetItem> createState() => _DropTargetItemState();
+}
+
+class _DropTargetItemState extends State<DropTargetItem> {
+  String dragItem = '';
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(flex: 1, child: Text(widget.property)),
+        Expanded(
+          child: DragTarget<String>(
+            builder: (context, candidateData, rejectedData) => Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: candidateData.isNotEmpty
+                    ? Border.all(color: Colors.red, width: 2)
+                    : null,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(dragItem.isEmpty ? 'Drop Here' : dragItem),
+                  ),
+                  if (dragItem.isNotEmpty)
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          dragItem = '';
+                        });
+                      },
+                      child: Icon(Icons.clear),
+                    ),
+                ],
+              ),
+            ),
+            onAccept: (data) {
+              setState(() {
+                if (dragItem.isEmpty) {
+                  dragItem = data;
+                } else {
+                  dragItem += '   $data';
+                }
+              });
+              widget.onDrop(widget.property, dragItem);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/utils/constant.dart
@@ -0,0 +1,10 @@
+abstract final class ContactProperties {
+  static const String name = 'Name';
+  static const String mobile = 'mobile';
+  static const String email = 'email';
+  static const String address = 'address';
+  static const String company = 'company';
+  static const String designation = 'designation';
+  static const String website = 'website';
+ 
+}
